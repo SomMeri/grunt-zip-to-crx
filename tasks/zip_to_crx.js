0a318fa26cda974e2080478c677a8bd4024ea52d
jurcovicovam <meri@meri.org>
2014-11-22 20:48:49 +0100

    An empty template that will contain zip-to-crx project later on.

---
+++ tasks/zip_to_crx.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-zip-to-crx
+ * https://github.com/SomMeri/grunt-zip-to-crx
+ *
+ * Copyright (c) 2014 Maria Jurcovicova
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('zip_to_crx', 'Converts zipped files into chrome extension file (.crx).', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
